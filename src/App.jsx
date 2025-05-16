ff18ae85356d5c241e4cf71fcbcdb8e3501523be
Pedr0135 <rm95165@estudante.fieb.edu.br>
2025-05-16 14:28:59 +0000

    Pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import {Routes, Route} from 'react-router-dom'
-import Home from ' ./Home'
-  const App = () => {
+import Home from './Home'
+ 
+const App = () => {
      
     return (
 
       <Routes>
 
-        <Route path='/' element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
 
       </Routes>
 
